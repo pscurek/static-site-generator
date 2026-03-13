0cd164639270215e6167e8204149a8a49d436a32
Paul Scurek <9504814+pscurek@users.noreply.github.com>
2026-03-13 13:25:56 -0500

    moved text_node_to_html_node() function to textnode.py and moved all associated tests to test_textnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,5 @@
 import unittest
-
-from htmlnode import HTMLNode, LeafNode, ParentNode, text_node_to_html_node
-from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_one_prop(self):
@@ -108,39 +106,6 @@ class TestParentNode(unittest.TestCase):
         actual = repr(parent_node)
         expected = "ParentNode(div, [LeafNode(span, child, None)], {'prop1': 'property1'})"
         self.assertEqual(actual, expected)
-
-class TestTextNodeToHTMLNode(unittest.TestCase):
-    def test_text(self):
-        node = TextNode("This is a text node", TextType.TEXT)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, None)
-        self.assertEqual(html_node.value, "This is a text node")
-
-    def test_link(self):
-        node = TextNode("anchor text", TextType.LINK, "https://example.com")
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "a")
-        self.assertEqual(html_node.value, "anchor text")
-        self.assertEqual(html_node.props, {"href": "https://example.com"})
-
-    def test_image(self):
-        node = TextNode("alt text", TextType.IMAGE, "https://example.com")
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "img")
-        self.assertEqual(html_node.value, None)
-        self.assertEqual(html_node.props, {"src": "https://example.com", "alt": "alt text"})
-
-    def test_code(self):
-        node = TextNode("some code", TextType.CODE)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "code")
-        self.assertEqual(html_node.value, "some code")
-        self.assertEqual(html_node.props, None)
-
-    def test_no_text_type(self):
-        node = TextNode("text", None)
-        with self.assertRaises(TypeError):
-            text_node_to_html_node(node)   
 
 if __name__ == "__main__":
     unittest.main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,5 @@
 import unittest
-
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -27,8 +26,39 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a link text node", TextType.LINK)
         self.assertNotEqual(node1, node2)
         self.assertFalse(node1.url == node2.url)
-        
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("anchor text", TextType.LINK, "https://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "anchor text")
+        self.assertEqual(html_node.props, {"href": "https://example.com"})
+
+    def test_image(self):
+        node = TextNode("alt text", TextType.IMAGE, "https://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, None)
+        self.assertEqual(html_node.props, {"src": "https://example.com", "alt": "alt text"})
+
+    def test_code(self):
+        node = TextNode("some code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "some code")
+        self.assertEqual(html_node.props, None)
+
+    def test_no_text_type(self):
+        node = TextNode("text", None)
+        with self.assertRaises(TypeError):
+            text_node_to_html_node(node)
+
 if __name__ == "__main__":
     unittest.main()
-
-
