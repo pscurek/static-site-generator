358f413bbf55f349b928d8ad9a6fd1cf5138d678
Paul Scurek <9504814+pscurek@users.noreply.github.com>
2026-03-27 10:46:05 -0500

    renamed copy_static_to_public() to copy_files() and refactored to take two argruments. updated main.py import statement and body accordingly

--- src/copystatic.py
+++ src/copystatic.py
@@ -2,28 +2,26 @@ import os
 import shutil
 from datetime import datetime
 
-def copy_static_to_public():
+def copy_files(source_path, dest_path):
     root = os.getcwd()
     if not root.endswith("static-site-generator"):
         raise Exception("not a valid path")
-    static_path = os.path.join(root, "static")
-    public_path = os.path.join(root, "public")
     log_path = os.path.join(root, f"logs/{datetime.now().strftime("%Y.%m.%d-%I.%M.%S")}-static_to_public.log")
 
     try:
         with open(log_path, "w") as f:
-            f.write(f"copying contents of {static_path} to {public_path} ...\n")
+            f.write(f"copying contents of {source_path} to {dest_path} ...\n")
     except Exception as e:
         print(e)
         return
 
-    if os.path.exists(public_path):
-        shutil.rmtree(public_path)
-        add_to_log(log_path, f"directory {public_path} removed recursively")
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path)
+        add_to_log(log_path, f"directory {dest_path} removed recursively")
 
-    os.mkdir(public_path)
-    add_to_log(log_path, f"directory {public_path} created")
-    recursive_copy(static_path, public_path, log_path)
+    os.mkdir(dest_path)
+    add_to_log(log_path, f"directory {dest_path} created")
+    recursive_copy(source_path, dest_path, log_path)
 
 def recursive_copy(source, destination, log_path):
     contents = os.listdir(source)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 from textnode import TextNode, TextType
-from copystatic import copy_static_to_public
+from copystatic import copy_files
 from page_generator import generate_pages_recursive
 
 def main():
     # node = TextNode("This is some anchor text", TextType.LINK, "https://www.example.com")
     # print(node)
 
-    copy_static_to_public()
-    generate_pages_recursive("content", "template.html", "public")
+    dir_path_static = "./static"
+    dir_path_public = "./public"
+    dir_path_content = "./content"
+    template_path = "./template.html"
+
+    copy_files(dir_path_static, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 if __name__ == "__main__":
     main()
